620f292d0822b0d0fbba62c0083e278f7bd0b076
adrienbkr <adrien.becker@gmail.com>
2020-05-18 16:06:09 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import countries from './countries';
+
+const ReactFlagsSelect = (props) => {
+	console.log(props, countries)
+	return (
+		<div>
+			coucou
+		</div>
+	)
+}
+
+export default ReactFlagsSelect;
